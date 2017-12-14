afbb02267be8afc9f1e213c19c8d29ca86d3eb9e
Shannon Moran <moranse@umich.edu>
2017-12-14 12:21:50 -0500

    Put together Networkx example

---
+++ 03_Project/code/SCC.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""Strongly connected components.
+"""
+#    Copyright (C) 2004-2013 by
+#    Aric Hagberg <hagberg@lanl.gov>
+#    Dan Schult <dschult@colgate.edu>
+#    Pieter Swart <swart@lanl.gov>
+#    All rights reserved.
+#    BSD license.
+import networkx as nx
+from networkx.utils.decorators import not_implemented_for
+__authors__ = "\n".join(['Eben Kenah',
+                         'Aric Hagberg (hagberg@lanl.gov)'
+                         'Christopher Ellison',
+                         'Ben Edwards (bedwards@cs.unm.edu)'])
+
+__all__ = ['number_strongly_connected_components',
+           'strongly_connected_components',
+           'strongly_connected_component_subgraphs',
+           'is_strongly_connected',
+           'strongly_connected_components_recursive',
+           'kosaraju_strongly_connected_components',
+           'condensation']
+
+def strongly_connected_components(G):
+    """Generate nodes in strongly connected components of graph.
+
+    Parameters
+    ----------
+    G : NetworkX Graph
+       An directed graph.
+
+    Returns
+    -------
+    comp : generator of lists
+       A list of nodes for each strongly connected component of G.
+
+    Raises
+    ------
+    NetworkXNotImplemented: If G is undirected.
+
+    See Also
+    --------
+    connected_components, weakly_connected_components
+
+    Notes
+    -----
+    Uses Tarjan's algorithm with Nuutila's modifications.
+    Nonrecursive version of algorithm.
+
+    References
+    ----------
+    .. [1] Depth-first search and linear graph algorithms, R. Tarjan
+       SIAM Journal of Computing 1(2):146-160, (1972).
+
+    .. [2] On finding the strongly connected components in a directed graph.
+       E. Nuutila and E. Soisalon-Soinen
+       Information Processing Letters 49(1): 9-14, (1994)..
+    """
+    preorder={}
+    lowlink={}
+    scc_found={}
+    scc_queue = []
+    i=0     # Preorder counter
+    for source in G:
+        if source not in scc_found:
+            queue=[source]
+            while queue:
+                v=queue[-1]
+                if v not in preorder:
+                    i=i+1
+                    preorder[v]=i
+                done=1
+                v_nbrs=G[v]
+                for w in v_nbrs:
+                    if w not in preorder:
+                        queue.append(w)
+                        done=0
+                        break
+                if done==1:
+                    lowlink[v]=preorder[v]
+                    for w in v_nbrs:
+                        if w not in scc_found:
+                            if preorder[w]>preorder[v]:
+                                lowlink[v]=min([lowlink[v],lowlink[w]])
+                            else:
+                                lowlink[v]=min([lowlink[v],preorder[w]])
+                    queue.pop()
+                    if lowlink[v]==preorder[v]:
+                        scc_found[v]=True
+                        scc=[v]
+                        while scc_queue and preorder[scc_queue[-1]]>preorder[v]:
+                            k=scc_queue.pop()
+                            scc_found[k]=True
+                            scc.append(k)
+                        yield scc
+                    else:
+                        scc_queue.append(v)
---
+++ 03_Project/code/graphs_nx.py
@@ -0,0 +1,73 @@
+# Import needed packages
+import time
+from tqdm import tqdm,tqdm_notebook
+import itertools
+import numpy as np
+import random
+import networkx as nx
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+
+class Graph(object):
+
+    def __init__(self,n,m,process_name):
+        self.n = int(n)
+        self.m = int(m)
+        self.n_list = np.linspace(0,self.n-1,self.n).astype(int)
+        self.edge_density = m/n
+        self.phi = self.edge_density/n
+        # tracks edges & orders them by when they were added
+        self.edges = np.zeros((n,n),dtype=np.dtype('f4'))
+        self.nodes = 0
+        self.LJ = 0
+        self.process = process_name
+
+    def build(self):
+        if self.process=='C-ODER': self.CODER()
+        elif self.process=='ODER': self.ODER()
+        elif self.process=='DER': self.DER()
+        else: print('build with a valid process')
+
+    def add_edge_to_matrix(self,p):
+        if self.edges[p[0],p[1]]:
+            if self.edges[p[1],p[0]]: pass
+            else: self.nodes+=1; self.edges[p[1],p[0]] = self.nodes; self.G.add_edge(p[1],p[0])
+        else: self.nodes+=1; self.edges[p[0],p[1]] = self.nodes; self.G.add_edge(p[0],p[1])
+
+    def CODER(self):
+        pbar = tqdm_notebook(total=(self.m-self.nodes),desc="Building C-ODER graph")
+        while self.nodes<self.m:
+            first_node, second_node, third_node = random.sample(list(self.n_list),3)
+            proposed_edges = tuple(itertools.combinations(tuple(sorted((first_node,second_node,third_node))), 2))
+            # for nodes with min difference, check if they/their reverse already exist; if no, add them
+            difference = np.asarray([nodes[1]-nodes[0] for nodes in proposed_edges])
+            idx = np.where(difference == difference.min())[0]
+            n0 = self.nodes
+            for i in idx:
+                proposed_edge = proposed_edges[i]
+                self.add_edge(proposed_edge)
+            pbar.update(self.nodes-n0)
+        pbar.close()
+
+    def ODER(self):
+        pbar = tqdm_notebook(total=(self.m-self.nodes),desc="Building ODER graph")
+        self.G = nx.DiGraph()
+        self.G.add_nodes_from(range(self.n))
+        while self.nodes<self.m:
+            first_node, second_node = random.sample(list(self.n_list),2)
+            proposed_edge = tuple(sorted((first_node,second_node)))
+            n0 = self.nodes
+            self.add_edge_to_matrix(proposed_edge)
+            pbar.update(self.nodes-n0)
+        pbar.close()
+
+    def DER(self):
+        pbar = tqdm_notebook(total=(self.m-self.nodes),desc="Building DER graph")
+        while self.nodes<self.m:
+            first_node, second_node = random.choice(list(self.n_list)),random.choice(list(self.n_list))
+            proposed_edge = tuple((first_node,second_node))
+            n0 = self.nodes
+            self.add_edge(proposed_edge)
+            pbar.update(self.nodes-n0)
+        pbar.close()
